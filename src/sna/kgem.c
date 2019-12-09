f66d39544bb8339130c96d282a80f87ca1606caf
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-12-09 17:01:36 +0200

    sna: Fix dirtyfb detection
    Fix the accidentally swapped bpp and depth values passed to
    the addfb ioctl when we're testing for dirtyfb presence.
    Currently the addfb fails every time so we don't even test
    the actual dirtyfb ioctl.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/kgem.c
+++ src/sna/kgem.c
@@ -1538,8 +1538,8 @@ static bool test_has_dirtyfb(struct kgem *kgem)
 	create.width = 32;
 	create.height = 32;
 	create.pitch = 4*32;
-	create.bpp = 24;
-	create.depth = 32; /* {bpp:24, depth:32} -> x8r8g8b8 */
+	create.bpp = 32;
+	create.depth = 24; /* {bpp:32, depth:24} -> x8r8g8b8 */
 	create.handle = gem_create(kgem->fd, 1);
 	if (create.handle == 0)
 		return false;
