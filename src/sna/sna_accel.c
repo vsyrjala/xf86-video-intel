vsyrjala/xf86-video-intel

commit 3d5a1238af6a98f1e8f4b41f0faeda5921c92c52

sna: Restore blt fallback backend
Despite suggestions to the contrary, the BLT survived for another year.

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/sna/sna_accel.c
@@ -18115,9 +18115,6 @@ static bool sna_option_accel_none(struct sna *sna)
 	if (!xf86ReturnOptValBool(sna->Options, OPTION_ACCEL_ENABLE, TRUE))
 		return true;
 
-	if (sna->kgem.gen >= 0120)
-		return true;
-
 	if (!intel_option_cast_to_bool(sna->Options,
 				       OPTION_ACCEL_METHOD,
 				       !IS_DEFAULT_ACCEL_METHOD(NOACCEL)))

commit 7181c5a41c3f00eaf996caa156523c708a18081e

sna: Avoid selecting gen9 backend for future gen
Cannonlake, then Icelake introduce new instruction formats and state
command, and require a new render backend to be written. Avoid selecting
the gen9 backend as this will hang!

Closes: https://gitlab.freedesktop.org/drm/intel/-/issues/1864
Fixes: 3d5a1238af6a ("sna: Restore blt fallback backend")
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/sna/sna_accel.c
@@ -18242,6 +18242,8 @@ bool sna_accel_init(ScreenPtr screen, struct sna *sna)
 		sna_render_mark_wedged(sna);
 	} else if (sna_option_accel_blt(sna))
 		(void)backend;
+	else if (sna->kgem.gen >= 0120)
+		(void)backend; /* no render backend written yet */
 	else if (sna->kgem.gen >= 0110)
 		backend = gen9_render_init(sna, backend);
 	else if (sna->kgem.gen >= 0100)